aef4d033364912f38220010241c7b1a2e02430fc
Crynner <calvin.shih19@gmail.com>
2025-05-19 15:01:12 +1200

    added comments to best of understanding

--- Best Hangman words/Hangman letter rarity.py
+++ Best Hangman words/Hangman letter rarity.py
@@ -20,9 +20,14 @@ class LetterValue:
 class WordScore:
 
     def __init__(self):
+        # initialise letter counter
         self.data = LetterValue("words_alpha.txt")
         self.data.alp_list_calculator()
+
+        # copy over all letters from letter counter, except unused chars, with inverted weight
         self.letter_scores = {ch: 1 / freq for ch, freq in self.data.alp_list.items() if freq > 0}
+
+        # other initialisers
         self.score =[]
         self.top_scorers = []
         self.penalty_letters = set('tnshrdlaeiou')
@@ -31,20 +36,23 @@ class WordScore:
     def score_word(self, repetition_factor=0.1, penalty_factor = 0.5):
         for word in self.data.file_data:
             
+            # skip words with blacklisted letter (set via __str__())
             if any(letter in self.blacklist for letter in word):
                     continue
             base_score = sum(self.letter_scores.get(ch, 0) for ch in word)
 
+            # gets count of each letter in word, and number of non-repeated chars (!=?)
             counts = Counter(word)
             repeats = sum(count for count in counts.values() if count == 1)
 
+            # add 10% for every non-repeating letter
             repetition_multiplier = 1 + (repeats * repetition_factor)
             score = base_score * repetition_multiplier
 
+            # account for penalty score (half score?)
             score -= score * penalty_factor
 
-            score = round(score * 100000, 3)
-
+            score = round(score * 100000, 3) # 100 000 trivially to approx to scores below 100
             self.score.append((word, score))
 
     def blacklist_sort(self):
@@ -55,10 +63,12 @@ class WordScore:
         for word, score in self.score:
             if len(word) == length:
                 correct_len_list.append((word, score))
+        # correct_len_list = [wordscore[0] for wordscore in self.score if len(wordscore[0]) == length]
 
         def get_score(item):
             return item[1]
         
+        # sort via highest-to-lowest score
         correct_len_list.sort(key=get_score, reverse=True)
 
         self.top_scorers = correct_len_list
@@ -66,7 +76,6 @@ class WordScore:
     def __str__(self):
         print('\n')
 
-        
         user_input = int(input("What length of word do you want: "))
         self.blacklist = input("Are there any letters you do want in there (if multiple letters, type them with no spacing): ")
         print("\n")
@@ -74,25 +83,28 @@ class WordScore:
         self.get_rarest(user_input)
         if self.top_scorers == []:
             return f"There are no words in the English dictionary with a length of {user_input} letters."
+        
+        # get rounded average of all scores
         total = 0
         for i in self.top_scorers:
             total += i[1]
         average = round(total / len(self.top_scorers), 3)
 
+        # word with closest score to average
         closest_word = min(
         self.top_scorers,
         key=lambda x: abs(x[1] - average)
         )
 
-        count = 1
+        # print top ten, followed by summary stats
         top_ten = self.top_scorers[:10]
-        for i in top_ten:
+        for count, i in enumerate(top_ten, start=1):
             print(f"No. {count} = {i[0]} with a score of {i[1]}")
-            count += 1
         print("\n")
         print(f"Total number of words with a size of {user_input} is {len(self.top_scorers)}, and the average score was {average}")
 
         return f"The closest word to the average is '{closest_word[0]}' with a score of {closest_word[1]}"
 
-ws = WordScore()
-print(ws)
+if __name__ == '__main__':
+    ws = WordScore()
+    print(ws)
--- Best Hangman words/consecutiveConsonants.py
+++ Best Hangman words/consecutiveConsonants.py
@@ -7,32 +7,43 @@ SOFT_VOWELS_S = 'aeiouy'
 
 class wordSpecies:
     def __init__(self, wordlength, vowelType):
+        # determines vowels to check (generally, 'y' inclusive or not)
         self.vowelType = vowelType
-        file = open(FILE_PATH, 'r')
-        self.wordlist = [x for x in file.read().splitlines() if len(x) == wordlength]
-        file.close()
 
-    def get_consecutive(self, wordnum = 10, trimDoubleLetter = False):
+        # grab all words of defined length, after trimming whitespace
+        with open(FILE_PATH, 'r') as file:
+            self.wordlist = [x.strip() for x in file.read().splitlines() if len(x.strip()) == wordlength]
+
+    def get_consecutive(self, wordnum = None, trimDoubleLetter = False):
         maxCon = 0
         maxConList = []
         for x in self.wordlist:
+
+            # if enabled, removes all consecutive instances of letters (e.g. converts "bully" into "buly")
             if trimDoubleLetter:
                 x = re.sub(f'([^{self.vowelType}])(?=(?:\\1))', '', x, flags=re.DOTALL)
+
+            # find largest consonant streak
             splitWord = re.split(f'[{self.vowelType}]+', x, flags=re.IGNORECASE)
             maxWordCon = max([len(y) for y in splitWord])
+
+            # if longer consonant streak found, wipe list and add word
             if maxWordCon > maxCon:
                 maxCon = maxWordCon
                 maxConList = [x]
+            # if equal consonant streak found, add to list
             elif maxWordCon == maxCon:
                 maxConList.append(x)
-        return (maxCon, maxConList[:wordnum-1]) # return wordnum number of words
+
+        return (maxCon, maxConList[:wordnum]) # return wordnum number of words
 
 
     def __str__(self):
+        # prints all words of initialised length
         return '\n'.join(self.wordlist)
     
 
 if __name__ == '__main__': # test example
     q = input("word length:\n>>> ")
-    w = wordSpecies(int(q), SOFT_VOWELS_S)
-    print(w.get_consecutive(10, True))
+    w = wordSpecies(int(q), HARD_VOWELS_S)
+    print(w.get_consecutive(trimDoubleLetter=True))
