d19b0216dd301fd3a74b0f779f14c00e820c11c4
Crynner <calvin.shih19@gmail.com>
2025-05-19 15:07:06 +1200

    add commented-out alternative statement to blacklisting all words with no vowels

--- Best Hangman words/file sorter.py
+++ Best Hangman words/file sorter.py
@@ -1,4 +1,4 @@
-
+""" Removes all words in a given file that does not contain any vowels (including y)"""
 def file_sorter(filename):
     with open(filename, "r+") as file:
         file_data = file.read().splitlines()
@@ -6,6 +6,9 @@ def file_sorter(filename):
         for word in file_data:
             word = word.lower()
             word_list = list(word)
+            # untested alternative
+            # if all(x not in word_list for x in ['a', 'e', 'i', 'o', 'u', 'y']):
+            #     blacklist_words.append(word)
             if "a" not in word_list:
                 if "e" not in word_list:
                     if "i" not in word_list:
