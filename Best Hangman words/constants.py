1ce9f9fdfc4764037579d499722c22e5e2b3d3b6
Crynner <calvin.shih19@gmail.com>
2025-05-22 10:36:23 +1200

    added beginning implementation of loading text

--- Best Hangman words/constants.py
+++ Best Hangman words/constants.py
@@ -1,6 +1,8 @@
 """Strings or values that may be used across multiple files."""
 
 from enum import Enum
+import sys
+import time
 
 WORDLIST_PATH = "words_alpha.txt"
 NON_ALPHA = r'''1234567890`~!@#$%^&*()_+-=[]{}\|;:'",.<>/?'''
@@ -21,6 +23,20 @@ class TerminalColours(Enum):
 
     # TODO more colour print variations
 
+class BufferText:
+    def __init__(self):
+        pass
+
+    def loading_animation(message="Processing"):
+        i = 0
+        while True:
+            dots = '.' * (i % 4)
+            print(f"\rTest{dots}   ", flush=True, end='')
+            time.sleep(0.5)
+            i += 1
+
 if __name__ == "__main__":
     TerminalColours.printColor(TerminalColours.PINK, "test text")
     TerminalColours.printColor(TerminalColours.RED, "test text")
+    q = BufferText()
+    q.loading_animation()
