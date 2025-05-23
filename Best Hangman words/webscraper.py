2c5b4adf6b07f64f8073611b170032656a72b19d
Dave-III <seakinsconnor@gmail.com>
2025-05-23 12:19:44 +1200

    updated webscraper, needs implimentation

--- Best Hangman words/webscraper.py
+++ Best Hangman words/webscraper.py
@@ -2,24 +2,20 @@ import requests
 from bs4 import BeautifulSoup as bs
 r = requests.get("https://www.dictionary.com/browse/thru")
 print(r)
-soup = bs(r.content, 'html.parser')
  
-s = soup.find('div', class_='NZKOFkdkcvYgD3lqOIJw')
-content = soup.find_all('h2')
-texts = [tag.get_text(strip=True) for tag in content]
-count = 0
-
-print()
-print(s)
-print()
-if ""
-
-for h2 in content:
-
-    if "abbreviation for" in h2:
-        count += 1
-
-if count > 0:
+if r.status_code == 404:
     print("invalid word")
 else:
-    print(content)
+    soup = bs(r.content, 'html.parser')
+
+    # Get all h2 tags and their text
+    content = soup.find_all('h2')
+    texts = [tag.get_text(strip=True).lower() for tag in content]
+
+    # Count how many h2 tags contain "abbreviation for"
+    count = sum(1 for text in texts if "abbreviation for" in text)
+
+    if count > 0:
+        print("invalid word")
+    else:
+        print("valid word")
